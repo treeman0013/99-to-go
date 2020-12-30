2191a220b970db8ac9f7beaa9d6c4993f6cedd1c
Alek Evans <contact@alekeagle.com>
2020-12-29 19:20:39 -0700

    lol

--- Scripts/MapGenerator.cs
+++ Scripts/MapGenerator.cs
@@ -3,28 +3,25 @@ using System;
 
 public class MapGenerator
 {
-    RandomNumberGenerator rng = new RandomNumberGenerator();
-    public Node Generate(int seed, Node map, Spatial[] rooms)
-    {
-        //set up seed and vars
-        rng.Seed = (ulong)seed;
-		int mapX = rng.RandiRange(5, 15);
-		int mapZ = rng.RandiRange(4, 12);
 
-        map.AddChild(rooms[0]);
-        
-        //generate map
-        for(int x = 0; x < mapX; x++)
-        {
-            for(int z = 0; z < mapZ; z++)
-            {
-                map.AddChild(rooms[0].Duplicate());
-                rooms[0].Translation = new Vector3(x * 20, 0, z * 20);
-                GD.Print($"Room Created at x:{x} and z:{z}");
-            }
-        }
+  RandomNumberGenerator rng = new RandomNumberGenerator();
+  public Node Generate(int seed, Node map, Spatial[] rooms)
+  {
+    //set up seed and vars
+    rng.Seed = (ulong)seed;
+    int mapX = rng.RandiRange(5, 15);
+    int mapZ = rng.RandiRange(4, 12);
 
-        GD.Print($"RoomX: {mapX} RoomY: {mapZ}");
-        return map;
-    }
+    //generate map
+    int ranX = rng.RandiRange(1, mapX - 1), ranZ = rng.RandiRange(1, mapZ - 1);
+
+    Spatial mainRoom = (Spatial)rooms[0].Duplicate();
+    mainRoom.Name = "Room_" + ranX + "," + ranZ;
+    map.AddChild(mainRoom);
+
+    map.GetNode<Spatial>("Room_" + ranX + "," + ranZ).Translation = new Vector3(ranX * 20, 0, ranZ * 20);
+
+    GD.Print($"{map.GetChildren()[0]}");
+    return map;
+  }
 }
